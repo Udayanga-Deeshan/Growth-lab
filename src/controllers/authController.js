6ba7e0b9b7f48dfd7e1c3df9684c1a974716180a
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2025-09-30 23:53:40 +0530

    Implement user sign-up functionality with validation and password hashing

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,28 @@
-export const signUp = (req,res) =>{
-    res.send('User signed up');
-}
+import bcrypt from "bcryptjs";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+export const signUp = async (req, res) => {
+  const { name, email, password } = req.body;
 
-export const login = (req,res) =>{
-    res.send('User logged in');
-}
+  try {
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    const hasedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: { name, email, password: hasedPassword, role }
+
+
+    });
+
+    const token = jwt.sign({id:user.id,role:user.role},process.env.JWT_SECRET,{expiresIn:'1h'});
+    res.status(201).json({ user: { id: user.id, name: user.name, email: user.email, role: user.role }, token });
+  } catch (error) {
+     res.status(500).json({ message: 'Something went wrong', error: err.message });
+  }
+};
+
+export const login = (req, res) => {
+  res.send("User logged in");
+};
